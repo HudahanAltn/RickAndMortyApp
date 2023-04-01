a223b593a9a49c5442ea8e8eb674232b16789e8c
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-04-01 17:55:46 +0300

    bazı constraint'ler eklendi

--- RickAndMorty/MainViewController.swift
+++ RickAndMorty/MainViewController.swift
@@ -7,8 +7,15 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController{
 
+   
+    @IBOutlet weak var collectionViewWorldTypes: UICollectionView!
+    @IBOutlet weak var tableViewCharacterTypes: UITableView!
+    
+    var temp = ["hüdahan","hüdahan2"]
+    var safeAreaWidth:CGFloat = 0.0
+    var safeAreaHeight:CGFloat = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,9 +27,49 @@ class MainViewController: UIViewController {
         }
         
         
-       
+    
+        tableViewCharacterTypes.delegate = self
+        tableViewCharacterTypes.dataSource = self
+        collectionViewWorldTypes.delegate = self
+        collectionViewWorldTypes.dataSource = self
     }
-
-
+    
+   
+ 
 }
 
+
+
+extension MainViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return temp.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "characterCell",for:indexPath)
+        cell.textLabel?.text = temp[indexPath.row]
+        return cell
+    }
+}
+
+extension MainViewController:UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "worldCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
