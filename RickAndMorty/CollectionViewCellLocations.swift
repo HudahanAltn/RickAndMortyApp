96d9348794067d1c3bc29e6ed9a794de7fe83eef
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-04-13 05:21:43 +0300

    collectionView yatay olacak şekilde tasarlandı.CV hücre tasarımı yapıldı. Yatay CV listesinde  Rick and Morty evreni dünyaları listelendi.Ufak grafik değişiklikleri yapıldı.

---
+++ RickAndMorty/CollectionViewCellLocations.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCellLocations.swift
+//  RickAndMorty
+//
+//  Created by Hüdahan Altun on 13.04.2023.
+//
+
+import UIKit
+
+class CollectionViewCellLocations: UICollectionViewCell {
+    
+    @IBOutlet weak var worldName: UILabel!
+}
--- RickAndMorty/MainViewController.swift
+++ RickAndMorty/MainViewController.swift
@@ -6,16 +6,33 @@
 //
 
 import UIKit
-
+import RickMortySwiftApi
 class MainViewController: UIViewController{
 
    
     @IBOutlet weak var collectionViewWorldTypes: UICollectionView!
     @IBOutlet weak var tableViewCharacterTypes: UITableView!
     
-    var temp = ["hüdahan","hüdahan2"]
-    var safeAreaWidth:CGFloat = 0.0
-    var safeAreaHeight:CGFloat = 0.0
+    var temp = ["hüdahan","hüdahan2"]//deneme listesi
+    
+    let rmClient = RMClient()//cliend başlatıldı
+    
+    var RMLocations:[RMLocationModel] = [RMLocationModel]()//url get isteğine bağlı olarak gelecek RM dünyalarının tutulacağı dizi.
+   
+    
+    func fetchDataLoc(completionHandler: @escaping (Result<[RMLocationModel], Error>) -> Void) {
+        //async fonksiyonun viewDidload içerisinde kullanılması için oluşturulan
+        Task {
+            do {
+                let locations = try await rmClient.location().getAllLocations()
+                completionHandler(.success(locations))
+                
+            } catch {
+                completionHandler(.failure(error))
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,8 +43,23 @@ class MainViewController: UIViewController{
             self.navigationItem.titleView = imageView
         }
         
+        //dünyalar viewdidload ile birlikte gelecek
+        fetchDataLoc { result in
+                switch result {
+                case .success(let locations):
+                    // Veri işleme işlemleri burada yapılır
+                    self.RMLocations = locations.shuffled()//rastgele sırada alıyoruz.
+                    self.collectionViewWorldTypes.reloadData()//CV güncellenecek
+                    print("lokasyonlar alındı: \(locations)")
+                case .failure(let error):
+                    print(error)
+                }
+            
+            }
         
-    
+        
+        structureCVCell()
+        //protocol bağlantıları
         tableViewCharacterTypes.delegate = self
         tableViewCharacterTypes.dataSource = self
         collectionViewWorldTypes.delegate = self
@@ -60,16 +92,51 @@ extension MainViewController:UITableViewDelegate,UITableViewDataSource{
     }
 }
 
-extension MainViewController:UICollectionViewDelegate,UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+extension MainViewController:UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "worldCell", for: indexPath)
-        
-        return cell
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return RMLocations.count
     }
     
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let location = RMLocations[indexPath.row]//location'ı al
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "worldCell", for: indexPath)
+        as! CollectionViewCellLocations
+        
+        cell.worldName.text = location.name//ismi cell'e yazdır.
+        
+        //Hücre içi label tasarımı
+        let genislik = self.collectionViewWorldTypes.frame.size.width
+        let maxSize = CGSize(width: genislik/2-5, height: 50)
+        let fitSize = cell.worldName.sizeThatFits(maxSize)
+        cell.worldName.frame.size = fitSize
+        cell.worldName.textAlignment = .center
+        
+        //Hücre etrafına border çizme
+        cell.layer.borderColor = UIColor.lightGray.cgColor
+        cell.layer.borderWidth = 0.5
+        return cell
+    
+    }
+    
+}
+
+extension MainViewController{
+    
+    func structureCVCell(){// CVdeki hücrenin tasarımı
+        let design:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        design.scrollDirection = .horizontal
+        let genislik = self.collectionViewWorldTypes.frame.size.width
+        design.sectionInset = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 1)
+        design.minimumLineSpacing = 5
+        design.minimumInteritemSpacing = 10
+        let hücreGenislik = genislik/2
+        design.itemSize = CGSize(width: hücreGenislik, height: 50)
+        collectionViewWorldTypes.collectionViewLayout = design
+    }
 }
